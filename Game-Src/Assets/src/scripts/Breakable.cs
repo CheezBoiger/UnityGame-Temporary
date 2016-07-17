e4adf23e42494cd573f007ce55a16926441de9cd
CheezBoiger <alexander.garcia18@yahoo.com>
2016-07-17 15:10:39 -0700

    Added new scripts and example scene

---
+++ Game-Src/Assets/src/scripts/Breakable.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace GameProject {
+
+	/// <summary>
+	/// Breakables define objects that are able to be broken by ActiveObjects.
+	/// All things involved in breakable objects (such as crates, walls, bushes, vases, etc)
+	/// should be associated with Breakable. Attributes are still in the works.
+	/// </summary>
+	public abstract class Breakable : Entity {
+
+		/// Use this for initialization
+		public abstract override void Start();
+		/// Update is called once per frame
+		public abstract override void Update();
+	}
+}
---
+++ Game-Src/Assets/src/scripts/Unbreakable.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace GameProject {
+	/// <summary>
+	/// Unbreakable defines all objects that cannot break, these include 
+	/// walls that define the bounds of the map, so that the play may not somehow
+	/// jump out of the map, and other materials that should not be destroyed during 
+	/// gameplay, by ActiveObjects.
+	/// </summary>
+	public abstract class Unbreakable : Entity {
+
+		// Use this for initialization
+		public abstract override void Start();
+	
+		// Update is called once per frame
+		public abstract override void Update();
+	}
+}
