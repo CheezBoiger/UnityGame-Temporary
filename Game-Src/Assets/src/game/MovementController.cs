8b77934f966c78e9a0697a4d1c0912e5d798dbfd
CheezBoiger <alexander.garcia18@yahoo.com>
2016-07-19 18:21:42 -0700

    Added Movement Scripts for Actors.

---
+++ Game-Src/Assets/src/game/MovementController.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace GameProject {
+	/// <summary>
+	/// Movement is a Free Moving Application Script, which allows ActiveObjects to 
+	/// be moveable by control. We can modify the movement speed by means of manipulation
+	/// from Items, but player would need to do this.
+	/// </summary>
+	public class MovementController : MonoBehaviour {
+		public float movementRate = 0.05f;
+
+		private Vector3 moveVector;
+
+		public float MovementRate {
+			get {
+				return MovementRate;
+			} set {
+				movementRate = value;
+			}
+		}
+
+		// Use this for initialization
+		void Start() {
+			
+		}
+
+		// Update is called once per frame
+		void Update() {
+			moveVector = this.transform.position;
+			Debug.Log("Object is moving!");		
+
+			if(Input.GetKey(KeyCode.W)) {
+				moveVector.z += movementRate;
+				moveVector.x += movementRate;
+			}
+			
+			if(Input.GetKey(KeyCode.S)) {
+				moveVector.z -= movementRate;
+				moveVector.x -= movementRate;
+			}
+
+			if(Input.GetKey(KeyCode.A)) {
+				moveVector.x -= movementRate;
+				moveVector.z += movementRate;
+			}
+
+			if(Input.GetKey(KeyCode.D)) {
+				moveVector.x += movementRate;
+				moveVector.z -= movementRate;
+			}
+
+			transform.position = moveVector;
+		}
+	}
+}
